695d0da8cf47adade913f6c41b413f41c8e1481a
Maxime Morin <maxime_morette@hotmail.com>
2025-10-15 19:24:43 -0400

    feat : fixed references and typos and added Unit class and ICombatEntity interface

--- Assets/Scripts/Runtime/Game/Deck.cs
+++ Assets/Scripts/Runtime/Game/Deck.cs
@@ -1,4 +1,3 @@
-using Codice.Client.Common.GameUI;
 using NUnit.Framework;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,12 +15,6 @@ namespace Unity.Template.Multiplayer.NGO.Runtime
             if (rng == null) { rng = new System.Random(); }
             Shuffle(); // shuffle all decks when they are created at game start
         }
-
-        //// Update is called once per frame
-        //void Update()
-        //{
-
-        //}
 
         public abstract Card Draw();
 
