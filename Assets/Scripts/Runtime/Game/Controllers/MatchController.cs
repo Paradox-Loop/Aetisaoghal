41ef0c3aaa7d3c87fad7a87877dd18466b0e7a78
Maxime Morin <maxime_morette@hotmail.com>
2025-11-18 21:44:02 -0500

    feat : Added event logic to effect, combine both script into one since one was redundant and added trigger management to matchcontroller

--- Assets/Scripts/Runtime/Game/Controllers/MatchController.cs
+++ Assets/Scripts/Runtime/Game/Controllers/MatchController.cs
@@ -1,10 +1,13 @@
+using System.Collections.Generic;
 using Unity.Netcode;
+using UnityEngine.Events;
 
 namespace Unity.Template.Multiplayer.NGO.Runtime
 {
     internal class MatchController : Controller<GameApplication>
     {
         MatchView View => App.View.Match;
+        public List<Effect> triggers;
 
         void Awake()
         {
@@ -21,6 +24,11 @@ namespace Unity.Template.Multiplayer.NGO.Runtime
         {
             RemoveListener<CountdownChangedEvent>(OnCountdownChanged);
             RemoveListener<WinButtonClickedEvent>(OnClientWinButtonClicked);
+
+            foreach (var trigger in triggers)
+            {
+                trigger.RemoveEffect();
+            }
         }
 
         void OnCountdownChanged(CountdownChangedEvent evt)
@@ -32,5 +40,30 @@ namespace Unity.Template.Multiplayer.NGO.Runtime
         {
             NetworkManager.Singleton.LocalClient.PlayerObject.GetComponent<Player>().OnPlayerAskedToWinServerRpc();
         }
+
+        public void AddTrigger(Effect trigger)
+        {
+            triggers.Add(trigger);
+            trigger.AddEffect();
+        }
+
+        public void RemoveTrigger(Effect trigger)
+        {
+            triggers.Remove(trigger);
+            trigger.RemoveEffect();
+        }
+
+        public void CheckTrigger(Effect trigger)
+        {
+            if(triggers.Contains(trigger))
+            {
+                ActivateTrigger(trigger);
+            }
+        }
+
+        void ActivateTrigger(Effect trigger)
+        {
+            trigger.ActivateTrigger();
+        } 
     }
 }
--- Assets/Scripts/Runtime/Game/Effect.cs
+++ Assets/Scripts/Runtime/Game/Effect.cs
@@ -1,15 +1,203 @@
-using UnityEngine;
-using Unity.Netcode;
-using NUnit.Framework;
 using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using Unity.Netcode;
 
 namespace Unity.Template.Multiplayer.NGO.Runtime
 {
     public class Effect : NetworkBehaviour
     {
-        public void Activate(EnumLibrary.Ranks rank, List<GameObject> targets)
-        {
+        public virtual void AddEffect() { }
+        public virtual void RemoveEffect() { }
+        public virtual void ActivateTrigger() { }
 
+        public virtual void Activate(EnumLibrary.Ranks rank, List<GameObject> targets){}
+
+        public class Ressource : Effect
+        {
+            UnityEvent RessourceEffectEvent;
+            private int amount;
+            private EnumLibrary.Ressources type;
+
+            public override void AddEffect()
+            {
+                RessourceEffectEvent.AddListener(RessourceTrigger);
+            }
+
+            public override void RemoveEffect()
+            {
+                RessourceEffectEvent.RemoveListener(RessourceTrigger);
+            }
+
+            public override void ActivateTrigger()
+            {
+                RessourceEffectEvent.Invoke();
+            }
+
+            void RessourceTrigger()
+            {
+
+            }
+        }
+
+        public class Healing : Effect
+        {
+            UnityEvent HealingEffectEvent;
+            private int amount;
+
+            public override void AddEffect()
+            {
+                HealingEffectEvent.AddListener(HealingTrigger);
+            }
+
+            public override void RemoveEffect()
+            {
+                HealingEffectEvent.RemoveListener(HealingTrigger);
+            }
+
+            public override void ActivateTrigger()
+            {
+                HealingEffectEvent.Invoke();
+            }
+
+            void HealingTrigger()
+            {
+
+            }
+        }
+
+        public class Damage : Effect
+        {
+            UnityEvent DamageEffectEvent;
+            private int amount;
+
+            public override void AddEffect()
+            {
+                DamageEffectEvent.AddListener(DamageTrigger);
+            }
+
+            public override void RemoveEffect()
+            {
+                DamageEffectEvent.RemoveListener(DamageTrigger);
+            }
+
+            public override void ActivateTrigger()
+            {
+                DamageEffectEvent.Invoke();
+            }
+
+            void DamageTrigger()
+            {
+
+            }
+        }
+
+        public class Buff : Effect
+        {
+            UnityEvent BuffEffectEvent;
+            private int power;
+            private int hp;
+            private List<Keywords> keywords;
+
+            public override void AddEffect()
+            {
+                BuffEffectEvent.AddListener(BuffTrigger);
+            }
+
+            public override void RemoveEffect()
+            {
+                BuffEffectEvent.RemoveListener(BuffTrigger);
+            }
+
+            public override void ActivateTrigger()
+            {
+                BuffEffectEvent.Invoke();
+            }
+
+            void BuffTrigger()
+            {
+
+            }
+        }
+
+        public class Debuff : Effect
+        {
+            UnityEvent DebuffEffectEvent;
+            private int power;
+            private int hp;
+            private List<Keywords> keywords;
+
+            public override void AddEffect()
+            {
+                DebuffEffectEvent.AddListener(DebuffTrigger);
+            }
+
+            public override void RemoveEffect()
+            {
+                DebuffEffectEvent.RemoveListener(DebuffTrigger);
+            }
+
+            public override void ActivateTrigger()
+            {
+                DebuffEffectEvent.Invoke();
+            }
+
+            void DebuffTrigger()
+            {
+
+            }
+        }
+
+        public class Draw : Effect
+        {
+            UnityEvent DrawEffectEvent;
+            private int amount;
+
+            public override void AddEffect()
+            {
+                DrawEffectEvent.AddListener(DrawTrigger);
+            }
+
+            public override void RemoveEffect()
+            {
+                DrawEffectEvent.RemoveListener(DrawTrigger);
+            }
+
+            public override void ActivateTrigger()
+            {
+                DrawEffectEvent.Invoke();
+            }
+
+            void DrawTrigger()
+            {
+
+            }
+        }
+
+        public class Discard : Effect
+        {
+            UnityEvent DiscardEffectEvent;
+            private int amount;
+
+            public override void AddEffect()
+            {
+                DiscardEffectEvent.AddListener(DiscardTrigger);
+            }
+
+            public override void RemoveEffect()
+            {
+                DiscardEffectEvent.RemoveListener(DiscardTrigger);
+            }
+
+            public override void ActivateTrigger()
+            {
+                DiscardEffectEvent.Invoke();
+            }
+
+            void DiscardTrigger()
+            {
+
+            }
         }
     }
 }
