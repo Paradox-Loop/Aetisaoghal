0bf5bfe86d36535e8e5052d76b2a0f3ad51f30d6
Maxime Morin <maxime_morette@hotmail.com>
2025-12-01 19:28:53 -0500

    fix : changed Card for up-to-date version

--- Assets/Scripts/Runtime/Game/Card.cs
+++ Assets/Scripts/Runtime/Game/Card.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using Unity.Netcode;
+using UnityEngine;
 using UnityEngine.UI;
 using Image = UnityEngine.UIElements.Image;
 
@@ -11,7 +12,7 @@ namespace Unity.Template.Multiplayer.NGO.Runtime
         public EnumLibrary.CardTypes cardType;
         public EnumLibrary.CardSubtypes subType;
         public int cost;
-        public string name;
+        public string cardName;
         public EnumLibrary.Ranks rank;
         public List<Effect> effects;
         public Zone currentZone;
@@ -27,7 +28,7 @@ namespace Unity.Template.Multiplayer.NGO.Runtime
         void DoEffect(Effect effect)
         {
             //TO-DO : replace with actual targeting target itself for now
-            effect.ActivateTrigger(rank, new List<GameObject> {gameObject});
+            effect.ActivateTrigger(rank, new List<GameObject> { gameObject });
         }
 
         public virtual void RankUp() { }
